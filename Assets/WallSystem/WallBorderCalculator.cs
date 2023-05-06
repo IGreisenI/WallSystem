649a24456974246bbf85762c6f4b93e4030810c2
IGreisenI <kstjepan4@gmail.com>
2023-05-06 14:19:57 +0300

    Create WallBorderCalculator class that inherits from IBorder interface

---
+++ Assets/WallSystem/WallBorderCalculator.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WallSystem.Interfaces;
+
+namespace WallSystem
+{
+    public class WallBorderCalculator : IBorder
+    {
+        public Vector3[] GetBorderPoints()
+        {
+
+        }
+    }
+}
