b13e27e56a9bb80a67500e1925eab685c984b4e3
IGreisenI <kstjepan4@gmail.com>
2023-05-08 13:32:03 +0300

    Add FloorPlan which has points of the walls

---
+++ Assets/WallSystem/FloorPlan.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WallSystem
+{
+    public struct FloorPlan
+    {
+        public List<Vector3> wallPoints;
+    }
+}
