4c9fa3fbf3c77d22854a6c9efb5d4436b4442767
IGreisenI <kstjepan4@gmail.com>
2023-05-08 20:22:52 +0300

    Refactor WallSegment class so it's easier to work with all points

--- Assets/WallSystem/WallSegment.cs
+++ Assets/WallSystem/WallSegment.cs
@@ -9,28 +9,46 @@ namespace WallSystem
     {
         [SerializeField] private Vector3 _firstGroundPoint;
         [SerializeField] private Vector3 _secondGroundPoint;
-        [SerializeField] private Vector3 _wallSegmentHeightVector;
+        [SerializeField] private float _wallSegmentHeight;
+
+        private Vector3 _wallSegmentHeightVector;
+        private List<Vector3> _allPoints;
 
         public void Init(Vector3 firstGroundPoint, Vector3 secondGroundPoint, float wallSegmentHeight)
         {
             _firstGroundPoint = firstGroundPoint;
             _secondGroundPoint = secondGroundPoint;
-            _wallSegmentHeightVector = new Vector3(0, wallSegmentHeight);
+            _wallSegmentHeight = wallSegmentHeight;
+            _wallSegmentHeightVector = new Vector3(0, _wallSegmentHeight);
+
+            CalculateAllPoints();
+        }
+
+        private void CalculateAllPoints()
+        {
+            _allPoints = new List<Vector3>
+            {
+                _firstGroundPoint,
+                _secondGroundPoint,
+                _secondGroundPoint + _wallSegmentHeightVector,
+                _firstGroundPoint + _wallSegmentHeightVector
+            };
         }
 
         public List<Vector3> GetAllPoints()
         {
-            return new List<Vector3> { _firstGroundPoint, _secondGroundPoint, _secondGroundPoint + _wallSegmentHeightVector, _firstGroundPoint + _wallSegmentHeightVector };
+            CalculateAllPoints();
+            return _allPoints;
         }
 
         public void DrawWallGizmos()
         {
             Gizmos.color = Color.green;
 
-            Gizmos.DrawLine(_firstGroundPoint, _secondGroundPoint);
-            Gizmos.DrawLine(_secondGroundPoint, _secondGroundPoint + _wallSegmentHeightVector);
-            Gizmos.DrawLine(_secondGroundPoint + _wallSegmentHeightVector, _firstGroundPoint + _wallSegmentHeightVector);
-            Gizmos.DrawLine(_firstGroundPoint + _wallSegmentHeightVector, _firstGroundPoint);
+            for(int i = 0; i < _allPoints.Count; i++)
+            {
+                Gizmos.DrawLine(_allPoints[i], _allPoints[(i + 1) % _allPoints.Count]);
+            }
         }
     }
 }
