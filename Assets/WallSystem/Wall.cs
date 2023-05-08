4c584fc94805fd00fd5983b6974ddd4550eaa844
IGreisenI <kstjepan4@gmail.com>
2023-05-08 15:21:49 +0300

    Add functions for adding wall segments and init the wall class

--- Assets/WallSystem/Wall.cs
+++ Assets/WallSystem/Wall.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using WallSystem.Interfaces;
 
 namespace WallSystem
 {
     public class Wall : MonoBehaviour
     {
-        [SerializeField] private float wallHeight;
-        [SerializeField] private List<WallSegment> wallSegments;
+        [SerializeField] private float _wallHeight;
+        [SerializeField] private List<WallSegment> _wallSegments = new();
 
-        [ContextMenu("Create Random Wall")]
-        public void CreateRandomWallFromBorder(IBorder border)
+        #region CACHE
+        WallSegment tempWallSegment;
+        #endregion
+
+        public void Init(float wallHeight)
         {
+            _wallHeight = wallHeight;
+            _wallSegments = new List<WallSegment>();
+        }
 
+        private void OnDrawGizmos()
+        {
+            foreach(WallSegment wallSegment in _wallSegments)
+            {
+                wallSegment.DrawWallGizmos();
+            }
+        }
+
+        public void AddWallSegment(Vector3 firstGroundPoint, Vector3 secondGroundPoint)
+        {
+            tempWallSegment = new GameObject("WallSegment").AddComponent<WallSegment>();
+            tempWallSegment.transform.parent = this.transform;
+
+            tempWallSegment.Init(firstGroundPoint, secondGroundPoint, _wallHeight);
+
+            _wallSegments.Add(tempWallSegment);
+        }
+
+        public void SetHeight(float wallHeight)
+        {
+            _wallHeight = wallHeight;
         }
     }
 }
