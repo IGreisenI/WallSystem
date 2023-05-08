94815ce6bf11cb04ba301344e85ff8482c2cce96
IGreisenI <kstjepan4@gmail.com>
2023-05-08 13:35:48 +0300

    Add WallSegment class which hold the points of the wall

---
+++ Assets/WallSystem/WallSegment.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WallSystem
+{
+    public class WallSegement
+    {
+        private Vector3 _firstGroundPoint;
+        private Vector3 _secondGroundPoint;
+        private Vector3 _wallSegmentHeightVector;
+
+        public WallSegement(Vector3 firstGroundPoint, Vector3 secondGroundPoint, float wallSegmentHeight)
+        {
+            _firstGroundPoint = firstGroundPoint;
+            _secondGroundPoint = secondGroundPoint;
+            _wallSegmentHeightVector = new Vector3(0, wallSegmentHeight);
+        }
+
+        public List<Vector3> GetAllPoints()
+        {
+            return new List<Vector3> { _firstGroundPoint, _secondGroundPoint, _secondGroundPoint + _wallSegmentHeightVector, _firstGroundPoint + _wallSegmentHeightVector };
+        }
+
+        public void DrawWallGizmos()
+        {
+            Gizmos.color = Color.green;
+
+            Gizmos.DrawLine(_firstGroundPoint, _secondGroundPoint);
+            Gizmos.DrawLine(_secondGroundPoint, _secondGroundPoint + _wallSegmentHeightVector);
+            Gizmos.DrawLine(_secondGroundPoint + _wallSegmentHeightVector, _firstGroundPoint + _wallSegmentHeightVector);
+            Gizmos.DrawLine(_firstGroundPoint + _wallSegmentHeightVector, _firstGroundPoint);
+
+        }
+    }
+}
