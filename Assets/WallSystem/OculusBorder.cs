b9ef0fb5f58697fa6e0e0077a5ad128531a6c0cc
IGreisenI <kstjepan4@gmail.com>
2023-05-08 22:59:44 +0300

    Add class for getting the border from Oculus devices

---
+++ Assets/WallSystem/OculusBorder.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WallSystem.Interfaces;
+
+namespace WallSystem 
+{
+    public class OculusBorder : IBorder
+    {
+
+        public List<Vector3> GetBorderPoints()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
